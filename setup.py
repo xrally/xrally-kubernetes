15bd0b31184d39ec8e61c85504f4ab594fe3dcdd
Andrey Kurilin <andr.kurilin@gmail.com>
2018-06-13 16:25:06 +0300

    Switch to setuptools-scm from pbr
    It gives us more control and flexibility.
    
    Also, this commit:
    * sets the proper home-url.
    * update short-description

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,17 +11,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
+import os
 import setuptools
+from setuptools import find_packages
 
-# In python < 2.7.4, a lazy loading of package `pbr` will break
-# setuptools if some other modules registered functions in `atexit`.
-# solution from: http://bugs.python.org/issue15881#msg170215
-try:
-    import multiprocessing  # noqa
-except ImportError:
-    pass
+
+def read_data(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    with open(filename) as f:
+        return f.read()
+
+
+def read_requirements(filename):
+    return [l for l in read_data(filename).split("\n")
+            if l and not l.startswith("#")]
+
+
+# we do not have extras yet.
+EXRAS_REQUIREMENTS = {}
+
 
 setuptools.setup(
-    setup_requires=['pbr>=1.8'],
-    pbr=True)
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
+    name="xrally-kubernetes",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    description="A set of xRally plugins to run workloads against Kubernetes "
+                "platform.",
+    long_description=read_data("README.md"),
+    long_description_content_type="text/markdown",
+    author="xRally Team",
+    url="https://xrally.org/plugins/kubernetes/overview",
+    license="Apache License, Version 2.0",
+    entry_points={
+        "rally_plugins": [
+            "path = xrally_kubernetes",
+            #"options = xrally_kubernetes.common.opts:list_opts"
+        ]
+    },
+    install_requires=read_requirements("requirements.txt"),
+    extras_require=EXRAS_REQUIREMENTS,
+    classifiers=["Intended Audience :: Developers",
+                 "Intended Audience :: Information Technology",
+                 "License :: OSI Approved :: Apache Software License",
+                 "Operating System :: POSIX :: Linux",
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 2",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3",
+                 "Programming Language :: Python :: 3.4",
+                 "Programming Language :: Python :: 3.5",
+                 "Programming Language :: Python :: 3.6"]
+)
